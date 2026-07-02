f5fd54428c08aaadc41eba4d87fb264109361da4
KaldaM <matteuskalda@gmail.com>
2026-07-02 21:29:48 +0300

    Set default outlet capacities by connector type

--- planner-core/src/main/java/ee/matteus/pannukas/core/model/ConnectorType.java
+++ planner-core/src/main/java/ee/matteus/pannukas/core/model/ConnectorType.java
@@ -1,17 +1,23 @@
 package ee.matteus.pannukas.core.model;
 
 public enum ConnectorType {
-    SCHUKO_230V("230V tavapesa"),
-    INDUSTRIAL_16A("16A tööstusvool"),
-    INDUSTRIAL_32A("32A tööstusvool");
+    SCHUKO_230V("230V tavapesa", 3500),
+    INDUSTRIAL_16A("16A tööstusvool", 11000),
+    INDUSTRIAL_32A("32A tööstusvool", 22000);
 
     private final String displayName;
+    private final int defaultCapacityWatts;
 
-    ConnectorType(String displayName) {
+    ConnectorType(String displayName, int defaultCapacityWatts) {
         this.displayName = displayName;
+        this.defaultCapacityWatts = defaultCapacityWatts;
     }
 
     public String displayName() {
         return displayName;
+    }
+
+    public int defaultCapacityWatts() {
+        return defaultCapacityWatts;
     }
 }
